889f408300fa80431c99800aa81a1c4c8955b880
Samuel Teuber <teuber@kit.edu>
2022-10-20 10:19:04 +0200

    Fixed python version

--- deps/build.jl
+++ deps/build.jl
@@ -7,6 +7,8 @@ pkg"build PyCall"
 @info "Loading Conda"
 using Conda
 @info "Installing packages necessary for NNEnum"
+Conda.add("certifi")
+Conda.add("python=3.8")
 Conda.pip_interop(true)
 Conda.pip("install",["numpy","scipy==1.7","threadpoolctl","onnx==1.9.0","onnxruntime==1.8.0","skl2onnx==1.7.0","swiglpk","termcolor"])
 Conda.pip("install",["cvc5"])
