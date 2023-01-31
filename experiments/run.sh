6eed2a8d555700627f2cff4faa72fbf3508e970e
Samuel Teuber <teuber@kit.edu>
2023-01-31 15:49:31 +0100

    Minor changes

--- experiments/run.sh
+++ experiments/run.sh
@@ -11,9 +11,43 @@ run_acc () {
 
     runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-1.jld > experiments/acc/$1/results-approx-1.log 2>&1
 
-    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-2.jld > experiments/acc/$1/results-approx-2.log 2>&1
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 2 test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-2.jld > experiments/acc/$1/results-approx-2.log 2>&1
 
-    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-3.jld > experiments/acc/$1/results-approx-3.log 2>&1
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 3 test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-3.jld > experiments/acc/$1/results-approx-3.log 2>&1
+}
+
+run_acc_two () {
+
+    echo "Running experiments for $1"
+
+    mkdir -p experiments/acc/$1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula-bounds2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-bounds.jld > experiments/acc/$1/results-bounds.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-1.jld > experiments/acc/$1/results-approx-1.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 2 test/parsing/examples/acc/formula2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-2.jld > experiments/acc/$1/results-approx-2.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 3 test/parsing/examples/acc/formula2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/results-approx-3.jld > experiments/acc/$1/results-approx-3.log 2>&1
+}
+
+run_acc_fallback () {
+
+    echo "Running experiments for $1"
+
+    mkdir -p experiments/acc/$1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula-fallback test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback-approx-1.jld > experiments/acc/$1/fallback-approx-1.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 2 test/parsing/examples/acc/formula-fallback test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback-approx-2.jld > experiments/acc/$1/fallback-approx-2.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 3 test/parsing/examples/acc/formula-fallback test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback-approx-3.jld > experiments/acc/$1/fallback-approx-3.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/acc/formula-fallback2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback2-approx-1.jld > experiments/acc/$1/fallback2-approx-1.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 2 test/parsing/examples/acc/formula-fallback2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback2-approx-2.jld > experiments/acc/$1/fallback2-approx-2.log 2>&1
+
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 3 test/parsing/examples/acc/formula-fallback2 test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/$1.onnx experiments/acc/$1/fallback2-approx-3.jld > experiments/acc/$1/fallback2-approx-3.log 2>&1
 }
 
 run_zeppelin () {
@@ -22,9 +56,9 @@ run_zeppelin () {
 
     mkdir -p experiments/zeppelin/$1
 
-    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/zeppelinAvoidanceSmallStateSpace/formula-bounds test/parsing/examples/zeppelinAvoidanceSmallStateSpace/fixed test/parsing/examples/zeppelinAvoidanceSmallStateSpace/mapping test/networks/$1.onnx experiments/$1/result-bounds.jld > result-bounds.out 2>&1
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/zeppelinAvoidanceSmallStateSpace/formula-bounds test/parsing/examples/zeppelinAvoidanceSmallStateSpace/fixed test/parsing/examples/zeppelinAvoidanceSmallStateSpace/mapping test/networks/$1.onnx experiments/zeppelin/$1/result-bounds.jld > experiments/zeppelin/$1/result-bounds.out 2>&1
 
-    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/zeppelinAvoidanceSmallStateSpace/formula-smaller-precise test/parsing/examples/zeppelinAvoidanceSmallStateSpace/fixed test/parsing/examples/zeppelinAvoidanceSmallStateSpace/mapping test/networks/$1.onnx experiments/$1/result-approx-1.jld > result-approx-1.out 2>&1
+    runlim ./bin/SNNT --smtfilter-timeout=4 --approx 1 test/parsing/examples/zeppelinAvoidanceSmallStateSpace/formula-smaller-precise test/parsing/examples/zeppelinAvoidanceSmallStateSpace/fixed test/parsing/examples/zeppelinAvoidanceSmallStateSpace/mapping test/networks/$1.onnx experiments/zeppelin/$1/result-approx-1.jld > experiments/zeppelin/$1/result-approx-1.out 2>&1
 }
 
 # Requires that SNNT has already been built
@@ -38,7 +72,7 @@ if [ ! -f "$NCUBEV_PATH/bin/SNNT" ]; then
     exit 1
 fi
 
-if [ ! -f "$(which asdfadf)" ]; then
+if [ ! -f "$(which runlim)" ]; then
     echo "Missing runlim, please install it first."
     exit 1
 fi
@@ -52,7 +86,11 @@ run_acc "acc-improved-2000000-64-64"
 
 run_acc "acc-2000000-64-64-64-64"
 
-run_acc "acc-2000000-64-64-64-64-retrain-100000-200000-0.9"
+run_acc_two "acc-2000000-64-64-64-64-retrain-100000-200000-0.9"
+
+echo "Running ACC fallback experiments"
+
+run_acc_fallback "acc-2000000-64-64-64-64-retrain-100000-200000-0.9"
 
 echo "Running Zeppelin experiments"
 
