6a75bfdfdd500ffea2b8ec2a79a4673547ddd78e
Samuel Teuber <teuber@kit.edu>
2024-05-20 10:35:41 +0200

    Script for Zeppelin

---
+++ experiments/zeppelin/run.sh
@@ -0,0 +1,11 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/../../
+
+run_ncubev () {
+  mkdir -p experiments/zeppelin/${2}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/zeppelinAvoidanceSmallStateSpace-fixedC/formula-smaller-precise test/parsing/examples/zeppelinAvoidanceSmallStateSpace-fixedC/fixed${1} test/parsing/examples/zeppelinAvoidanceSmallStateSpace-fixedC/mapping test/networks/${2}.onnx experiments/zeppelin/${2}/result${1}-approx-1-RERUN --approx 1 --no-cores --no-normalization > experiments/zeppelin/${2}/result${1}-approx-1-RERUN.log 2>&1
+}
+
+run_ncubev "40" "zeppelin-1400000-8-8"
+#run_ncubev "40" "zeppelin-1400000-retrain-1000000-0.5-8-8"
