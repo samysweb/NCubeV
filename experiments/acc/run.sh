a25941103298320d1b9e7d839b3fcef11dfaf65a
Samuel Teuber <teuber@kit.edu>
2024-05-20 12:02:50 +0200

    ACC experiments rerun

---
+++ experiments/acc/run.sh
@@ -0,0 +1,50 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/../../
+
+run_ncubev () {
+  mkdir -p experiments/acc/${1}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/${1}.onnx experiments/acc/${1}/results-approx-${2}-RERUN --approx ${2} > experiments/acc/${1}/results-approx-${2}-RERUN.log 2>&1
+}
+
+run_ncubev_fallback () {
+  mkdir -p experiments/acc/${1}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/${1}.onnx experiments/acc/${1}/results-approx-${2}-RERUN --approx ${2} > experiments/acc/${1}/results-approx-${2}-RERUN.log 2>&1
+}
+
+run_ncubev_fallback () {
+  mkdir -p experiments/acc/${1}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/acc/formula-fallback test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/${1}.onnx experiments/acc/${1}/fallback-approx-${2}-RERUN --approx ${2} > experiments/acc/${1}/fallback-approx-${2}-RERUN.log 2>&1
+}
+
+run_ncubev_linear () {
+  mkdir -p experiments/acc/${1}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/acc/formula test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/${1}.onnx experiments/acc/${1}/results-linear-RERUN --linear > experiments/acc/${1}/results-linear-RERUN.log 2>&1
+}
+
+run_ncubev_fallback_linear () {
+  mkdir -p experiments/acc/${1}
+  OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 runlim ./deps/NCubeV/bin/NCubeV test/parsing/examples/acc/formula-fallback test/parsing/examples/acc/fixed test/parsing/examples/acc/mapping test/networks/${1}.onnx experiments/acc/${1}/fallback-linear-RERUN --approx 1 --linear > experiments/acc/${1}/fallback-linear-RERUN.log 2>&1
+}
+
+# run_ncubev_linear "acc-2000000-64-64-64-64"
+# run_ncubev "acc-2000000-64-64-64-64" "1"
+# run_ncubev "acc-2000000-64-64-64-64" "2"
+# run_ncubev "acc-2000000-64-64-64-64" "3"
+
+# run_ncubev_linear "acc-2000000-64-64-64-64-retrain-100000-200000-0.9"
+# run_ncubev "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "1"
+# run_ncubev "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "2"
+# run_ncubev "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "3"
+run_ncubev_fallback "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "1"
+# run_ncubev_fallback "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "2"
+# run_ncubev_fallback "acc-2000000-64-64-64-64-retrain-100000-200000-0.9" "3"
+
+# run_ncubev_linear "acc-improved-2000000-64-64"
+# run_ncubev "acc-improved-2000000-64-64" "1"
+# run_ncubev "acc-improved-2000000-64-64" "2"
+# run_ncubev "acc-improved-2000000-64-64" "3"
+
+run_ncubev_fallback_linear "acc-2000000-64-64-64-64-retrain-100000-200000-0.9"
+
+
