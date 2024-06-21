15a9ebf352dc45d6f33f1f7c7106a9509bc8e94c
Samuel Teuber <teuber@kit.edu>
2024-06-21 10:47:18 +0200

    Made submodules pull part of build

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ if [ -z $1 ]; then
     echo "Usage: ./build.sh <path-to-julia-executable>"
     exit 1
 fi
+echo "Pulling Submodules..."
+git submodule init
+git submodule update --recursive
+
 echo "Building NCubeV with $1"
 
 bash -c "OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 $1 -E 'using Pkg; Pkg.activate(\".\"); Pkg.instantiate(); Pkg.build();'"
