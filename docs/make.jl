ec7df7d6982aca5bb51fa28087d6cfbc8165ce91
Samuel Teuber <teuber@kit.edu>
2025-11-10 13:26:20 +0100

    Documentation publishing try 9

--- docs/make.jl
+++ docs/make.jl
@@ -42,5 +42,6 @@ makedocs(
 # Optional: deploy via GitHub Actions (disabled by default)
 deploydocs(
     repo = "github.com/samysweb/NCubeV.git",
+    devbranch="cleanup-0.10",
     branch = "gh-pages"
 )
